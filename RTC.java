9e94e2e9aa71c1b387aca0255fffcb27aa6cbf92
ShitalKandel <126435253+ShitalKandel@users.noreply.github.com>
2023-08-05 09:01:47 +0545

    Add files via upload
    This is another method for random group creation where we use array to take input for number of student from users as well as output

--- RTC.java
+++ RTC.java
@@ -6,46 +6,51 @@ import java.io.*;
 public class RTC {
     public static void main(String[] args) {
 
-        //we assign stdline
+        //we create a scanner to take user input
         Scanner scanner = new Scanner(System.in);
-    //constructor for  class list
+
+    //Initialize a list to hold user input
         List<String> rollnum = RollNum();
 
+        System.out.println("Enter the number of students:");
+        //taking input from users for the number of students
+
+        int numStudents = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+
+        //generate roll number for the number of students
+        rollnum = new ArrayList<>();
+        for (int i = 1; i <= numStudents; i++) {
+            rollnum.add("RollNum." + i);
+        }
+
+        //taking input from users to prompt the number of groups
         System.out.println("Enter the required number of Groups:");
         int numGroups = scanner.nextInt();
 
+        //creating random groups from the provided input for students and groups
         List<List<String>> groups = createRandomGroup(rollnum, numGroups);
 
+        //storing the created random group on filewriter named as team_group.txt
         String filename = storegroup(groups, "team_groups.txt");
 
+        //print message to show successfull creation and store of group
         System.out.println("Random teams created and stored in '" + filename + "'");
     }
-//assigning rollnum on a list
-    public static List<String> RollNum() {
-        List<String> rollnum = new ArrayList<>();
 
-        rollnum.add("RollNum.1");
-        rollnum.add("RollNum.2");
-        rollnum.add("RollNum.3");
-        rollnum.add("RollNum.4");
-        rollnum.add("RollNum.5");
-        rollnum.add("RollNum.6");
-        rollnum.add("RollNum.7");
-        rollnum.add("RollNum.8");
-        rollnum.add("RollNum.9");
-        rollnum.add("RollNum.10");
-        rollnum.add("RollNum.11");
-        rollnum.add("RollNum.12");
-        rollnum.add("RollNum.13");
-        rollnum.add("RollNum.14");
-
-        return rollnum;
+    //method to generate list of RollNUm
+    private static List<String> RollNum() {
+        return null; // we can even modify this method to return a predefined list
     }
 
+
+    //method to create random group from list of rollNumbers
     private static List<List<String>> createRandomGroup(List<String> rollnum, int numGroup) {
         List<List<String>> groups = new ArrayList<>();
         Random random = new Random();
-//Making input from CLI for (int) num of group
+
+        //shuffle the role number randomly
         List<String> shufflerollforteam = new ArrayList<>(rollnum);
         for (int i = shufflerollforteam.size() - 1; i > 0; i--) {
             int j = random.nextInt(i + 1);
@@ -54,10 +59,12 @@ public class RTC {
             shufflerollforteam.set(j, s);
         }
 
+        //calculate the number of rollnumber per group and remaining one
         int rollNumGroup = shufflerollforteam.size() / numGroup;
         int leftrollnum = shufflerollforteam.size() % numGroup;
         int startIndex = 0;
 
+        //create group and assign roll number to each group
         for (int i = 0; i < numGroup; i++) {
             int endIndex = startIndex + rollNumGroup;
 
@@ -74,7 +81,8 @@ public class RTC {
 
         return groups;
     }
-//string output on filewriter
+
+    //method to store the created ranodm group in text file
     private static String storegroup(List<List<String>> createRandomGroup, String filename) {
         try (FileWriter writer = new FileWriter(filename)) {
             for (int i = 0; i < createRandomGroup.size(); i++) {
